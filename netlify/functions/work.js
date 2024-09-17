89d02c376a2e8d73c808e9b3d52b4f2369318e43
MarconiSe <hnrygmz@gmail.com>
2024-09-16 19:09:40 -0600

    finished reading from database

--- netlify/functions/work.js
+++ netlify/functions/work.js
@@ -1,8 +1,16 @@
+const { MongoClient } = require("mongodb")
+
 const handler = async () => {
+  const client = new MongoClient(process.env.CONNECTIONSTRING)
+  await client.connect()
+
+  const recentWorkSection = await client.db().collection("RecentWorkSection").find().toArray()
+  client.close()
+
   return {
     statusCode: 200,
-    headers: {"Content-Type": "text/plain"},
-    body: "marconi".toUpperCase()
+    headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
+    body: JSON.stringify(recentWorkSection)
   }
 }
 
